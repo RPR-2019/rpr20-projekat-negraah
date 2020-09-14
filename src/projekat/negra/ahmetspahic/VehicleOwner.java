86f644470c6396e3379cb696ed3f8b8e0c594dad
Negra Ahmetspahic <nahmetspah1@etf.unsa.ba>
2020-09-14 13:56:15 +0200

    Added VehicleOwner

---
+++ src/projekat/negra/ahmetspahic/VehicleOwner.java
@@ -0,0 +1,72 @@
+package projekat.negra.ahmetspahic;
+
+import java.time.LocalDate;
+
+public class VehicleOwner {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private Integer upin; //Unique Personal Identification Number
+    private String adress;
+    private String phoneNumber;
+
+    public VehicleOwner() {
+    }
+
+    public VehicleOwner(String firstName, String lastName, LocalDate dateOfBirth, Integer upin, String adress, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.upin = upin;
+        this.adress = adress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getUpin() {
+        return upin;
+    }
+
+    public void setUpin(Integer upin) {
+        this.upin = upin;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
