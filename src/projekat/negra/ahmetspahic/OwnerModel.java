e73e1f505908d971f7b3eaabe891d0e44b89d81e
Negra Ahmetspahic <nahmetspah1@etf.unsa.ba>
2020-09-15 16:07:30 +0200

    Added a model class for VehicleOwner

---
+++ src/projekat/negra/ahmetspahic/OwnerModel.java
@@ -0,0 +1,111 @@
+package projekat.negra.ahmetspahic;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.FileChooser;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OwnerModel {
+    private ObservableList<VehicleOwner> allOwners = FXCollections.observableArrayList();
+    private SimpleObjectProperty<VehicleOwner> currentOwner = new SimpleObjectProperty<>();
+    private Connection conn;
+
+    public OwnerModel() {
+        conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:baza.db");
+        } catch (SQLException e) {
+        }
+    }
+
+    public void napuni() {
+            for (VehicleOwner owner : VehiclesDAO.getInstance().getVehicleOwnerList()) {
+                allOwners.add(owner);
+            }
+
+        currentOwner.set(null);
+    }
+
+    public ObservableList<VehicleOwner> getAllOwners() {
+        return allOwners;
+    }
+
+    public void setAllOwners(ObservableList<VehicleOwner> allOwners) {
+        this.allOwners = allOwners;
+    }
+
+    public VehicleOwner getCurrentOwner() {
+        return currentOwner.get();
+    }
+
+    public SimpleObjectProperty<VehicleOwner> currentOwnerProperty() {
+        return currentOwner;
+    }
+
+    public void setCurrentOwner(VehicleOwner currentOwner) {
+        if(this.currentOwner != null && this.currentOwner.get() != null) {
+            VehiclesDAO.getInstance().updateVehicleOwner(this.currentOwner.get());
+        }
+        this.currentOwner.set(currentOwner);
+    }
+
+    public void setCurrentOwner(int i) {
+        if(this.currentOwner != null && this.currentOwner.get() != null) {
+            VehiclesDAO.getInstance().updateVehicleOwner(this.currentOwner.get());
+        }
+        this.currentOwner.set(allOwners.get(i));
+    }
+
+    public void disconnect() {
+        if(this.currentOwner != null && this.currentOwner.get() != null) {
+            VehiclesDAO.getInstance().updateVehicleOwner(this.currentOwner.get());
+        }
+    }
+
+    public void deleteCurrent() {
+        VehiclesDAO.getInstance().deleteOwner(currentOwner.get().getId());
+    }
+
+    // TODO popraviti ovo
+    public void zapisiDatoteku(File f) {
+        if (f == null) {
+                return;
+        } else {
+            try {
+                Statement statement = conn.createStatement();
+                ResultSet r = statement.executeQuery("select * from korisnik");
+                PrintWriter printWriter = new PrintWriter(f);
+                while (r.next()) {
+                    printWriter.println(r.getString(5) + ":" + r.getString(6) + ":" + r.getInt(1) + ":" + r.getInt(1) + ":" + r.getString(2)
+                            + " " + r.getString(3) + ":" + ":");
+                }
+                printWriter.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
